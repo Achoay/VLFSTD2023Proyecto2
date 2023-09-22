42eeb482e9c85662f11ae715c63ca2851774473c
Achoay <clg80@me.com>
2023-09-22 17:45:57 +0200

    Añadido boton Powera pantalla

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+function turnScreenOn(){
+    let screen = document.getElementById("screenOn");
+    screen.classList.add("turnOn");
+    
+}
+
+function transparent(){
+    let main = document.getElementById("noColour");
+    main.classList.add("makeTransparent");
+    
+}
